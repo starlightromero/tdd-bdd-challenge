7dab1987716e166ac79fcc3cef47a7ab47f11775
Starlight Romero <starlightromero@protonmail.com>
2021-02-03 18:12:41 -0800

    [update] finished testing

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,6 +1,6 @@
-const mocha = require("mocha")
-const chai = require("chai")
-const utils = require("../utils")
+const chai = require('chai')
+const mocha = require('mocha')
+const utils = require('../utils')
 const expect = chai.expect
 
 // ========================================================
@@ -9,10 +9,10 @@ const expect = chai.expect
 // Lambdas lexically bind this and cannot access the Mocha context.
 // ========================================================
 
-it("should say hello", function() {
+it('should say hello', function () {
   const hello = utils.sayHello()
-  expect(hello).to.be.a("string")
-  expect(hello).to.equal("Hello")
+  expect(hello).to.be.a('string')
+  expect(hello).to.equal('Hello')
   expect(hello).with.lengthOf(5)
 })
 
@@ -26,8 +26,38 @@ it("should say hello", function() {
 // This is called "Red-Green-Refactor"
 // ========================================================
 
+it('should return the area of a rectangle', function () {
+  const area = utils.area(3, 4)
+  expect(area).to.be.a('number')
+  expect(area).to.equal(12)
+})
 
+it('should return area as null', function () {
+  const area = utils.area(-1, 10)
+  expect(area).to.be.null
+})
 
+it('should return the perimeter of a rectangle', function () {
+  const perimeter = utils.perimeter(3, 4)
+  expect(perimeter).to.be.a('number')
+  expect(perimeter).to.equal(14)
+})
+
+it('should return perimeter as null', function () {
+  const perimeter = utils.perimeter(-1, 10)
+  expect(perimeter).to.be.null
+})
+
+it('should return the radius of a circle', function () {
+  const circleArea = utils.circleArea(3)
+  expect(circleArea).to.be.a('number')
+  expect(circleArea).to.equal((Math.PI * 3) ** 2)
+})
+
+it('should return radius as null', function () {
+  const circleArea = utils.circleArea(-1)
+  expect(circleArea).to.be.null
+})
 
 // ========================================================
 // Level 2 Challenges
@@ -42,28 +72,79 @@ beforeEach((done) => {
   done()
 })
 
-it("Should create a new (object) Item with name and price", function() {
-  const item = utils.createItem("apple", 0.99)
-  expect(item).to.be.a("object")
-  expect(item).to.have.property("name", "apple")
-  expect(item).to.have.property("price", 0.99)
-  expect(item).to.have.property("quantity", 1)
+it('Should create a new (object) Item with name and price', function () {
+  const item = utils.createItem('apple', 0.99)
+  expect(item).to.be.a('object')
+  expect(item).to.have.property('name', 'apple')
+  expect(item).to.have.property('price', 0.99)
+  expect(item).to.have.property('quantity', 1)
 })
 
-it("Should return an array containing all items in cart")
+it('Should return an array containing all items in cart', function () {
+  const item = utils.createItem('orange', 0.79)
+  utils.addItemToCart(item)
+  const cart = utils.getShoppingCart()
+  expect(cart).to.be.a('array')
+  expect(cart).to.have.lengthOf(1)
+})
 
-it("Should add a new item to the shopping cart")
+it('Should add a new item to the shopping cart', function () {
+  const item = utils.createItem('orange', 0.79)
+  utils.addItemToCart(item)
+  const cart = utils.getShoppingCart()
+  expect(cart).to.be.a('array')
+  expect(cart).to.have.lengthOf(1)
+})
 
-it("Should return the number of items in the cart")
+it('Should return the number of items in the cart', function () {
+  const item = utils.createItem('orange', 0.79)
+  utils.addItemToCart(item)
+  const length = utils.getNumItemsInCart()
+  expect(length).to.be.a('number')
+  expect(length).to.equal(1)
+})
 
-it("Should remove items from cart")
+it('Should remove items from cart', function () {
+  const orange = utils.createItem('orange', 0.79)
+  const plum = utils.createItem('plum', 1.29)
+  utils.addItemToCart(orange)
+  utils.addItemToCart(plum)
+  const removedItem = utils.removeItemFromCart()
+  const cart = utils.getShoppingCart()
+  expect(removedItem).to.be.a('object')
+  expect(cart).to.be.a('array')
+  expect(cart).to.have.lengthOf(1)
+})
 
 // ========================================================
 // Stretch Challenges
 // ========================================================
 
-it("Should update the count of items in the cart")
+it('Should update the count of items in the cart', function () {
+  const cart = utils.getShoppingCart()
+  expect(cart).to.be.a('array')
+  expect(cart).to.have.lengthOf(0)
+  const orange = utils.createItem('orange', 0.79)
+  utils.addItemToCart(orange)
+  expect(cart).to.have.lengthOf(1)
+  const plum = utils.createItem('plum', 1.29)
+  utils.addItemToCart(plum)
+  expect(cart).to.have.lengthOf(2)
+})
 
-it("Should validate that an empty cart has 0 items")
+it('Should validate that an empty cart has 0 items', function () {
+  const cart = utils.getShoppingCart()
+  expect(cart).to.be.a('array')
+  expect(cart).to.have.lengthOf(0)
+})
 
-it("Should return the total cost of all items in the cart")
+it('Should return the total cost of all items in the cart', function () {
+  const orange = utils.createItem('orange', 0.79)
+  const plum = utils.createItem('plum', 1.29)
+  utils.addItemToCart(orange)
+  utils.addItemToCart(plum)
+  const total = utils.getCartTotal()
+  const cart = utils.getShoppingCart()
+  expect(total).to.be.a('number')
+  expect(total).to.equal(1.29 + 0.79)
+})
--- utils.js
+++ utils.js
@@ -3,20 +3,14 @@
 // ========================================================
 
 const sayHello = () => {
-  return "Hello"
+  return 'Hello'
 }
 
-const area = (w, h) => {
-  // should return the area
-}
+const area = (w, h) => w * h >= 0 ? w * h : null
 
-const perimeter = (w, h) => {
-  // should return the perimeter
-}
+const perimeter = (w, h) => w >= 0 && h >= 0 ? w * 2 + h * 2 : null
 
-const circleArea = r => {
-  // should return the area of the circle
-}
+const circleArea = r => r >= 0 ? (Math.PI * r) ** 2 : null
 
 // ========================================================
 // Level 2 Challenges
@@ -35,24 +29,41 @@ const createItem = (name, price) => {
   return { name, price, quantity: 1 }
 }
 
-const getShoppingCart = () => {
-  // should return the current state of shopping cart
-}
+const getShoppingCart = () => shoppingCart
 
-const addItemToCart = (item) => {
-  // should add item to shopping cart
-}
+const addItemToCart = item => shoppingCart.push(item)
 
 const getNumItemsInCart = () => {
-  // should return the total quantity of items in cart
+  let numItems = 0
+  for (const item of shoppingCart) {
+    numItems += item.quantity
+  }
+  return numItems
 }
 
-const removeItemFromCart = (item) => {
-  // should remove item from shopping cart
+const removeItemFromCart = item => {
+  const index = shoppingCart.findIndex(el => el === item)
+  return shoppingCart.splice(index, 1)[0]
+}
+
+const getCartTotal = () => {
+  let total = 0
+  for (const item of shoppingCart) {
+    total += item.price
+  }
+  return total
 }
 
 module.exports = {
-  sayHello, area, perimeter, circleArea,
-  clearCart, createItem, getShoppingCart, addItemToCart,
-  getNumItemsInCart, removeItemFromCart
+  sayHello,
+  area,
+  perimeter,
+  circleArea,
+  clearCart,
+  createItem,
+  getShoppingCart,
+  addItemToCart,
+  getNumItemsInCart,
+  removeItemFromCart,
+  getCartTotal
 }
